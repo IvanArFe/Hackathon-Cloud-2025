78e634b9f199a8c6f4f60c45a394aa1c1c774853
Josep Lluís Marin Trabalón <101903899+josepLlMt20@users.noreply.github.com>
2025-02-15 03:43:59 +0100

    saveData

--- saveData.py
+++ saveData.py
@@ -1,28 +1,47 @@
 import os
-import sys
-from pymongo.mongo_client import MongoClient
+import pandas as pd
+from pymongo import MongoClient
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='psswd.env')
 
-# Get the MONGO_URI from the environment
-MONGO_URI = os.getenv("MONGO_URI")
+def save_csv_to_mongo(csv_filename):
+    load_dotenv(dotenv_path='psswd.env')
+    MONGO_URI = os.getenv("MONGO_URI")
 
-print(f'DATABASE_URL: {MONGO_URI}')
+    if not MONGO_URI:
+        print("Error: MONGO_URI not found in environment variables.")
+        return
 
-# Create a new client and connect to the server
-client = MongoClient(MONGO_URI)
+    client = MongoClient(MONGO_URI)
+    try:
+        client.admin.command('ping')
+        print("Connected to MongoDB successfully!")
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+        # Buscar el archivo CSV en el sistema
+        if not os.path.exists(csv_filename):
+            print(f"Error: File '{csv_filename}' not found.")
+            return
 
-    # add an element to the collection
-    db = client['hackato-urv']
-    collection = db['local']
-    data = {"name": "John", "age": 30}
-    collection.insert_one(data)
-    print("Data inserted successfully")
-except Exception as e:
-    print(e)
+        # Leer el archivo CSV con separador ';'
+        df = pd.read_csv(csv_filename, delimiter=';')
+        data = df.to_dict(orient='records')
+
+        # Obtener el nombre del archivo sin la extensión
+        collection_name = os.path.splitext(os.path.basename(csv_filename))[0]
+
+        # Insertar datos en MongoDB
+        db = client['hackato-urv']
+        collection = db[collection_name]
+        collection.insert_many(data)
+        print(f"Data from '{csv_filename}' inserted successfully into collection '{collection_name}' in MongoDB.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    csv_files = [f for f in os.listdir() if f.endswith(".csv")]
+    for file in csv_files:
+        save_csv_to_mongo(file)
